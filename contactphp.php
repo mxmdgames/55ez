d126d671cc74d7034b888538db3f19334c1a4d71
mxmdgames <100034864+mxmdgames@users.noreply.github.com>
2023-03-01 09:53:54 -0700

    Add files via upload

---
+++ contactphp.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_POST['submit'])) {
+  $to = "your_email@example.com";
+  $subject = "New Message from " . $_POST['name'];
+  $message = "Name: " . $_POST['name'] . "\nEmail: " . $_POST['email'] . "\n\n" . $_POST['message'];
+  $headers = "From: " . $_POST['email'] . "\r\n" . "Reply-To: " . $_POST['email'] . "\r\n" . "X-Mailer: PHP/" . phpversion();
+
+  mail($to, $subject, $message, $headers);
+  echo "Thank you for contacting me. Your message has been sent!";
+}
+?>
+
