e6703e63101a862254597d70743c1b6c33361962
mxmdgames <100034864+mxmdgames@users.noreply.github.com>
2023-03-01 09:55:05 -0700

    Update contactphp.php

--- contactphp.php
+++ contactphp.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST['submit'])) {
-  $to = "your_email@example.com";
+  $to = "warrenmadx@icloud.com";
   $subject = "New Message from " . $_POST['name'];
   $message = "Name: " . $_POST['name'] . "\nEmail: " . $_POST['email'] . "\n\n" . $_POST['message'];
   $headers = "From: " . $_POST['email'] . "\r\n" . "Reply-To: " . $_POST['email'] . "\r\n" . "X-Mailer: PHP/" . phpversion();
